0d34d511f23afd1c9688a0757e392883c70500f4
Cheng <cheng.li@rea-group.com>
2018-09-12 22:46:18 +0800

    Fix style issue

--- src/components/login-form/components/View.js
+++ src/components/login-form/components/View.js
@@ -3,16 +3,16 @@ import { Redirect } from 'react-router-dom';
 import '../style.scss';
 
 export default ({onLogin, logged}) => (
-  <div class="login-form">
+  <div className="login-form">
     {
       !logged &&
-      <div class="login-form__panel">
-        <div class="login-form__section">
+      <div className="login-form__panel">
+        <div className="login-form__section">
           <label htmlFor="username">Username:</label>
           <input ref={(ref) => this.username = ref} id="username" />
         </div>
-        <div class="login-form__section">
-          <label for="password">Password:</label>
+        <div className="login-form__section">
+          <label htmlFor="password">Password:</label>
           <input ref={(ref) => this.password = ref} type="password" id="password" />
         </div>
         <button className="login-form__button" onClick={() => {
